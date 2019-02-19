e5902e065b333922830b39a579f513e812f48ec2
SN MADDULA <snmaddula@outlook.com>
2019-02-19 18:52:01 +0530

    Create App.java

---
+++ src/main/java/snmaddula/quicktricks/redis/App.java
@@ -0,0 +1,20 @@
+package snmaddula.quicktricks.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * 
+ * @author snmaddula
+ *
+ */
+@EnableCaching
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
